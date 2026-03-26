40824d1552184f38a6d47278d9fbaa41dfe22cd6
nayoung.kim <jenny6816@naver.com>
2026-03-26 16:12:02 +0900

    프로젝트 세팅

---
+++ Challenge1_Hanchi/Challenge1_HanchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1_HanchiApp.swift
+//  Challenge1_Hanchi
+//
+//  Created by 김나영 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1_HanchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1_Hanchi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge1_Hanchi
+//
+//  Created by 김나영 on 3/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
